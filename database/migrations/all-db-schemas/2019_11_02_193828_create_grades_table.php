80c9e07ad724241cbb562ef8a991a010bda0a755
eliud niebles <eliudniebles@gmail.com>
2019-11-09 12:50:30 -0500

    se crean los tablas de la base de datos por el orm

---
+++ database/migrations/all-db-schemas/2019_11_02_193828_create_grades_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGradesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        
+        Schema::create('grades', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('descrition');
+            $table->bigInteger('grade_category_id');
+            $table->foreign('grade_category_id')->references('id')->on('grade_categories');
+            $table->timestamps();
+        });
+        Schema::enableForeignKeyConstraints();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('grades');
+    }
+}
